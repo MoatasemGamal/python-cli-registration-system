6d8adcfb5469d86b3a7bed7115b2d3d6ebffe359
MoatasemGamal <moatasemco@gmail.com>
2023-11-16 09:32:59 +0200

    create a singleton container
    to manage users in list and file

--- Classes.py
+++ Classes.py
@@ -4,3 +4,16 @@ class User:
         self.email=email
         self.password=password
 
+class UsersContainerSingleton:
+    __singleton = None
+
+    def __init__(self):
+        if UsersContainerSingleton.__singleton != None:
+            raise Exception("This singleton instantiated before !")
+        self.__users = []
+        UsersContainerSingleton.__singleton=self
+    @classmethod
+    def get_instance(cls):
+        if cls.__singleton==None:
+            UsersContainerSingleton()
+        return cls.__singleton
