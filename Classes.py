81e8fca2a87c86255a54e88356843e998642ef96
MoatasemGamal <moatasemco@gmail.com>
2023-11-16 10:18:01 +0200

    implement create_user(), load_users(), store_users()
    -create_user() method to create user and append to the container
    -load_users() method to load from the file
    -store_users() method to store the users data in the file

--- Classes.py
+++ Classes.py
@@ -17,3 +17,27 @@ class UsersContainerSingleton:
         if cls.__singleton==None:
             UsersContainerSingleton()
         return cls.__singleton
+
+    def create_user(self, name, email, password):
+        user = User(name, email, password)
+        self.__users.append(user)
+
+    def store_users(self):
+        file = open("./users.txt", "w")
+        lines=[]
+        for user in self.__users:
+            line = "|".join([user.name, user.email, user.password])
+            lines.append(line+"\n")
+        file.writelines(lines)
+        file.close()
+
+    def load_users(self):
+        file = open("./users.txt")
+        for line in file:
+            line=line.strip("\n")
+            line = line.split("|")
+            name     = line[0]
+            email    = line[1]
+            password = line[2]
+            self.__users.append(User(name, email, password))
+        file.close()
