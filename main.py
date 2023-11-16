bee910d56b3084b7ef9b8c70e8f77515f7ed0a65
MoatasemGamal <moatasemco@gmail.com>
2023-11-16 11:17:33 +0200

    create the container and load users in it

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 from Classes import User, UsersContainerSingleton, Auth
 import pyfiglet
+
+# get the singleton instance from UsersContainerSingleton class
+container = UsersContainerSingleton.get_instance()
+# load data from the users.txt file
+container.load_users()
